74d5ca49fff758cdb6246dd514c213f56307baa8
abdulalam <85445443+abdulalam@users.noreply.github.com>
2023-03-28 21:50:04 +0100

    Update Sssv1.py

--- Sssv1.py
+++ Sssv1.py
@@ -6,7 +6,7 @@ import xlsxwriter
 df = pd.read_csv('data.csv')
 print(df.head())
 shelf_list_with_stock = []
-for row in df["货架号"]:
+for row in df["Shelf No."]:
     if row[-1] != "z":
         shelf_list_with_stock.append(row)
 # print(shelf_list_with_stock)
