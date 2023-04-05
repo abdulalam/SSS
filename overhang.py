289472888c77d38912b4746b9b0960c2eb09a916
abdulalam <85445443+abdulalam@users.noreply.github.com>
2023-04-05 21:14:09 +0100

    Update overhang.py

--- overhang.py
+++ overhang.py
@@ -4,10 +4,10 @@ import xlsxwriter
 df = pd.read_csv('data.csv')
 
 
-new_df = df[["Shelf No.","Length","Width","Height"]]
-vol_df = df[["Shelf No.","Length","Width","Height","Available Qty."]]
+new_df = df[["Shelf No.","Long","Width","High"]]
+vol_df = df[["Shelf No.","Long","Width","High","Available Qty."]]
 
-vol_df["Volume"] = vol_df["Length"] * vol_df["Width"] * vol_df["Height"] * vol_df["Available Qty."] /1000000
+vol_df["Volume"] = vol_df["Long"] * vol_df["Width"] * vol_df["High"] * vol_df["Available Qty."] /1000000
 #vol_df["Volume"] = vol_df.iloc[:,1:5].multiply(axis=1)
 
 #print(vol_df.head)
@@ -17,7 +17,7 @@ vol_df["Volume"] = vol_df["Length"] * vol_df["Width"] * vol_df["Height"] * vol_d
 
 overhang = []
 for index, row in new_df.iterrows():
-    if row["Length"] > 120 or row["Width"] > 120 or row["Height"] > 120 :
+    if row["Long"] > 120 or row["Width"] > 120 or row["High"] > 120 :
         overhang.append(row["Shelf No."])
 
 overvol = []
@@ -30,4 +30,5 @@ percentage = (len(overhang) / len(new_df)) * 100
 print(f"Overhang : {percentage}% at {len(overhang)} pallets out of {len(new_df)}")
 percentage_v = (len(overvol) / len(vol_df)) * 100
 print(f"Overvol : {percentage_v}% at {len(overvol)} pallets out of {len(vol_df)}")
+
 
